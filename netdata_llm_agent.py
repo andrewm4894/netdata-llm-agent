6b2e95d5cbba81289500d720330caf082fa67739
Andrew Maguire <andrewm4894@gmail.com>
2025-02-01 19:35:20 +0000

    clean up a bit, add allmetrics funcion

--- netdata_llm_agent.py
+++ netdata_llm_agent.py
@@ -4,28 +4,28 @@ from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.tools import tool
 
 from netdata_tools import (
-    get_netdata_info,
-    get_netdata_charts,
-    get_netdata_chart_info,
-    get_netdata_chart_data,
-    get_netdata_alarms
+    get_info,
+    get_charts,
+    get_chart_info,
+    get_chart_data,
+    get_alarms
 )
 
 SYSTEM_PROMPT = """
 You are are helpful Netdata assistant. Users can ask you about Netdata charts, chart info, and chart data.
 
 The following tools are available:
-- get_netdata_info(netdata_host_url) : Get Netdata info.
-- get_netdata_charts(netdata_host_url) : Get Netdata charts.
-- get_netdata_chart_info(netdata_host_url, chart) : Get Netdata chart info for a specific chart.
-- get_netdata_chart_data(netdata_host_url, chart, after, before, points) : Get Netdata chart data for a specific chart.
-- get_netdata_alarms(netdata_host_url) : Get Netdata alarms.
+- get_info(netdata_host_url) : Get Netdata info.
+- get_charts(netdata_host_url) : Get Netdata charts.
+- get_chart_info(netdata_host_url, chart) : Get Netdata chart info for a specific chart.
+- get_chart_data(netdata_host_url, chart, after, before, points) : Get Netdata chart data for a specific chart.
+- get_alarms(netdata_host_url) : Get Netdata alarms.
 
 General Notes:
-- When pulling data from get_netdata_chart_data() you can leverage the points param to aggregate data points given the specific after and before time range.
+- When pulling data from get_chart_data() you can leverage the points param to aggregate data points given the specific after and before time range.
 - When there are multiple mirrored hosts you can adapt the base url to reflect the specific host you want to pull data from if the user asks about one of the mirrored hosts.
-- Charts with breakouts per user typically live at user.* eg. user.cpu_utilization, user.mem_usage etc. as per get_netdata_charts().
-- Charts with breakouts per application typically live at app.* eg. app.cpu_utilization, app.mem_usage etc. as per get_netdata_charts().
+- Charts with breakouts per user typically live at user.* eg. user.cpu_utilization, user.mem_usage etc. as per get_charts().
+- Charts with breakouts per application typically live at app.* eg. app.cpu_utilization, app.mem_usage etc. as per get_charts().
 """
 
 
@@ -52,11 +52,11 @@ class NetdataLLMAgent:
         self.platform = platform
         self.llm = ChatOpenAI(model=model) if platform == "openai" else ValueError("Only openai platform is supported.")
         self.tools = [
-            tool(get_netdata_info, parse_docstring=True),
-            tool(get_netdata_charts, parse_docstring=True),
-            tool(get_netdata_chart_info, parse_docstring=True),
-            tool(get_netdata_chart_data, parse_docstring=True),
-            tool(get_netdata_alarms, parse_docstring=True),
+            tool(get_info, parse_docstring=True),
+            tool(get_charts, parse_docstring=True),
+            tool(get_chart_info, parse_docstring=True),
+            tool(get_chart_data, parse_docstring=True),
+            tool(get_alarms, parse_docstring=True),
         ]
 
         self.agent = create_react_agent(
--- netdata_tools.py
+++ netdata_tools.py
@@ -3,7 +3,7 @@ import requests
 import pandas as pd
 
 
-def get_netdata_info(netdata_host_url: str) -> str:
+def get_info(netdata_host_url: str) -> str:
     """
     Calls Netdata /api/v1/info to retrieve system info and some metadata.
 
@@ -18,24 +18,28 @@ def get_netdata_info(netdata_host_url: str) -> str:
     r_json = resp.json()
 
     info = {
-        "netdata_version": r_json['version'],
-        "hostname": r_json['mirrored_hosts'][0],
-        "operating_system": r_json['os_name'],
-        "operating_system_version": r_json['os_id'],
-        "cores_total": r_json['cores_total'],
-        "total_disk_space": r_json['total_disk_space'],
-        "ram_total": r_json['ram_total'],
-        "mirrored_hosts_status": r_json['mirrored_hosts_status'],
-        "alarms": r_json['alarms'],
-        "buildinfo": r_json['buildinfo'],
-        "charts-count": r_json['charts-count'],
-        "metrics-count": r_json['metrics-count'],
+        "netdata_version": r_json["version"],
+        "hostname": r_json["mirrored_hosts"][0],
+        "operating_system": r_json["os_name"],
+        "operating_system_version": r_json["os_id"],
+        "cores_total": r_json["cores_total"],
+        "total_disk_space": r_json["total_disk_space"],
+        "ram_total": r_json["ram_total"],
+        "mirrored_hosts": [
+            {k: h[k] for k in ["hostname", "hops", "reachable"]}
+            for h in r_json["mirrored_hosts_status"]
+        ],
+        "alarms": r_json["alarms"],
+        "buildinfo": r_json["buildinfo"],
+        "charts-count": r_json["charts-count"],
+        "metrics-count": r_json["metrics-count"],
+        "collectors": r_json["collectors"],
     }
 
     return json.dumps(info, indent=2)
 
 
-def get_netdata_charts(netdata_host_url: str) -> str:
+def get_charts(netdata_host_url: str) -> str:
     """
     Calls Netdata /api/v1/charts to retrieve the list of available charts and some metadata about each chart.
 
@@ -43,23 +47,22 @@ def get_netdata_charts(netdata_host_url: str) -> str:
         netdata_host_url: Netdata host url to call.
 
     Returns:
-        JSON string with chart metadata.
+        JSON string with chart metadata. List of tuples with chart id and title.
     """
     url = f"{netdata_host_url}/api/v1/charts"
     resp = requests.get(url, timeout=5)
     r_json = resp.json()
 
-    charts = {}
+    charts = []
     for chart in r_json["charts"]:
-        charts[chart] = {
-            "name": r_json["charts"][chart]["name"],
-            "title": r_json["charts"][chart]["title"],
-        }
+        charts.append(
+            (r_json["charts"][chart]["name"], r_json["charts"][chart]["title"])
+        )
 
     return json.dumps(charts, indent=2)
 
 
-def get_netdata_chart_info(netdata_host_url: str, chart: str = 'system.cpu') -> str:
+def get_chart_info(netdata_host_url: str, chart: str = "system.cpu") -> str:
     """
     Calls Netdata /api/v1/chart?chart={chart} to retrieve info for a specific chart.
 
@@ -87,15 +90,16 @@ def get_netdata_chart_info(netdata_host_url: str, chart: str = 'system.cpu') -> 
     return json.dumps(chart_info, indent=2)
 
 
-def get_netdata_chart_data(
-        netdata_host_url: str,
-        chart: str = 'system.cpu',
-        after: int = -60,
-        before: int = 0,
-        points: int = 60
-    ) -> str:
+def get_chart_data(
+    netdata_host_url: str,
+    chart: str = "system.cpu",
+    after: int = -60,
+    before: int = 0,
+    points: int = 60,
+    df_freq: str = "5s",
+) -> str:
     """
-    Calls Netdata /api/v1/data?chart=chart for a specific chart.
+    Calls Netdata /api/v1/data?chart=chart for a specific chart over a specific time range.
 
     Args:
         netdata_host_url: Netdata host url.
@@ -103,9 +107,10 @@ def get_netdata_chart_data(
         after: Seconds before now or timestamp in seconds.
         before: Seconds after now or timestamp in seconds.
         points: Number of points to retrieve. Can be used to aggregate data.
+        df_freq: Frequency for the pandas DataFrame. Can be used to resample and aggregate data for larger time ranges.
 
     Returns:
-        JSON string with chart data.
+        Pandas DataFrame as a string with the chart data.
     """
     url = f"{netdata_host_url}/api/v1/data"
     query_params = {
@@ -118,11 +123,14 @@ def get_netdata_chart_data(
     resp = requests.get(url, params=query_params, timeout=5)
     resp_json = resp.json()
     df = pd.DataFrame(resp_json["data"], columns=resp_json["labels"])
+    df["time"] = pd.to_datetime(df["time"], unit="s")
+    df = df.set_index("time")
+    df = df.asfreq(df_freq)
 
     return df.to_string(index=False)
 
 
-def get_netdata_alarms(netdata_host_url: str) -> str:
+def get_alarms(netdata_host_url: str) -> str:
     """
     Calls Netdata /api/v1/alarms to retrieve the list of active alarms.
 
@@ -146,3 +154,29 @@ def get_netdata_alarms(netdata_host_url: str) -> str:
         }
 
     return json.dumps(alarms, indent=2)
+
+
+def get_current_metrics(netdata_host_url: str) -> str:
+    """
+    Calls Netdata /api/v1/allmetrics to retrieve current values for all metrics.
+
+    Args:
+        netdata_host_url: Netdata host url.
+
+    Returns:
+        JSON string with all metrics and their current values.
+    """
+    url = f"{netdata_host_url}/api/v1/allmetrics?format=json"
+    resp = requests.get(url, timeout=5)
+    r_json = resp.json()
+    all_metrics = {
+        c: {
+            "units": r_json[c]["units"],
+            "dimensions": {
+                d: r_json[c]["dimensions"][d]["value"] for d in r_json[c]["dimensions"]
+            },
+        }
+        for c in r_json
+    }
+
+    return json.dumps(all_metrics, indent=2)
